535a5c2df920ae01dbb6f31ce2ffaea39489dcb7
Wagner Bugs <wagnerbugs@gmail.com>
2024-06-10 16:09:19 -0300

    fix: list professionals

--- resources/views/tables/columns/process-detail-professionals.blade.php
+++ resources/views/tables/columns/process-detail-professionals.blade.php
@@ -7,17 +7,19 @@
     <div class="flex -space-x-4 rtl:space-x-reverse">
         @if ($getRecord()->details)
             @foreach ($getRecord()->details as $detail)
-                @foreach ($detail->professionals as $professionalId)
-                    @php
-                        $user = \App\Models\User::find($professionalId);
-                        if ($user) {
-                            $avatar = $user->profile && $user->profile->avatar ? asset('storage/' . $user->profile->avatar) : 'https://ui-avatars.com/api/?name=' . str_replace(' ', '+', $user->name);
-                        }
-                    @endphp
-                    @if ($user)
-                        <img class="h-10 w-10 rounded-full border-2 border-white dark:border-gray-800" src="{{ $avatar }}" alt="{{ $user->name }}" title="{{ $user->name }}">
-                    @endif
-                @endforeach
+                @if (isset($detail->professionals))
+                    @foreach ($detail->professionals as $professionalId)
+                        @php
+                            $user = \App\Models\User::find($professionalId);
+                            if ($user) {
+                                $avatar = $user->profile && $user->profile->avatar ? asset('storage/' . $user->profile->avatar) : 'https://ui-avatars.com/api/?name=' . str_replace(' ', '+', $user->name);
+                            }
+                        @endphp
+                        @if ($user)
+                            <img class="h-10 w-10 rounded-full border-2 border-white dark:border-gray-800" src="{{ $avatar }}" alt="{{ $user->name }}" title="{{ $user->name }}">
+                        @endif
+                    @endforeach
+                @endif
             @endforeach
         @endif
     </div>
